91cf09ea5c1c66d03eda70b3153f313e209377b0
Bram Driesen <bramdriesen94@gnmail.com>
2021-07-15 12:01:04 +0200

    feat(composer): Added .env information and scripthandler

---
+++ scripts/composer/ScriptHandler.php
@@ -0,0 +1,44 @@
+
+<?php
+
+/**
+ * @file
+ * Contains \WordpressProject\composer\ScriptHandler.
+ */
+
+namespace WordpressProject\composer;
+
+use Composer\Script\Event;
+use Composer\Semver\Comparator;
+
+class ScriptHandler {
+
+  /**
+   * Checks if the installed version of Composer is compatible.
+   *
+   * @see https://github.com/composer/composer/pull/5035
+   */
+  public static function checkComposerVersion(Event $event) {
+    $composer = $event->getComposer();
+    $io = $event->getIO();
+
+    $version = $composer::VERSION;
+
+    // The dev-channel of composer uses the git revision as version number,
+    // try to the branch alias instead.
+    if (preg_match('/^[0-9a-f]{40}$/i', $version)) {
+      $version = $composer::BRANCH_ALIAS_VERSION;
+    }
+
+    // If Composer is installed through git we have no easy way to determine if
+    // it is new enough, just display a warning.
+    if ($version === '@package_version@' || $version === '@package_branch_alias_version@') {
+      $io->writeError('<warning>You are running a development version of Composer. If you experience problems, please update Composer to the latest stable version.</warning>');
+    }
+    elseif (Comparator::lessThan($version, '1.0.0')) {
+      $io->writeError('<error>Wordpress-project requires Composer version 1.0.0 or higher. Please update your Composer before continuing</error>.');
+      exit(1);
+    }
+  }
+
+}
